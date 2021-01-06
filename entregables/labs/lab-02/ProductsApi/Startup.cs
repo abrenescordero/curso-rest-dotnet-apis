dd96e7e235aa7d2fc38152123f2001739d1fe130
Estiven Brenes <steve.nbc@hotmail.es>
2021-01-05 20:51:19 -0600

    Pase de laboratorios a versiones

---
+++ entregables/labs/lab-02/ProductsApi/Startup.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+
+namespace ProductsApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Middleware
+            services.AddSingleton<Repositories.ProductRepository>();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: "CORS",
+                                  builder =>
+                                  {
+                                      builder.WithOrigins("https://localhost:80")
+                                             .AllowAnyHeader()
+                                             .AllowAnyMethod() // GET, POST, PUT, etc
+                                             .AllowCredentials();
+                                  });
+            });
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProductsApi", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors("CORS");
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ProductsApi v1"));
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ entregables/labs/lab-03/ProductsApi/Repositories/ProductRepository.cs
@@ -0,0 +1,49 @@
+﻿using ProductsApi.Data;
+using ProductsApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Repositories
+{
+    public class ProductRepository
+    {
+        //private readonly List<Product> _products;
+        private readonly AdventureWorksDbContext _context;
+        public ProductRepository(AdventureWorksDbContext context)
+        {
+            _context = context;
+            //_products =  new List<Product>(new[] { new Product { Id = 1, Name = "Pants" } });
+        }
+
+        public Product[] Get() 
+        {
+            return _context.Products.ToArray();
+        }
+
+        public Product Add(Product value)
+        {
+            _context.Products.Add(value);
+
+            _context.SaveChanges();
+
+            return value;
+        }
+
+        public void Delete(int id)
+        {
+            var match = _context.Products.Find(id);
+
+            if (match != null)
+            {
+                _context.Products.Remove(match);
+                _context.SaveChanges();
+            }
+        }
+
+        public Product Get(int id)
+        {
+            return _context.Products.FirstOrDefault(p => p.Id == id);
+        }
+    }
+}
